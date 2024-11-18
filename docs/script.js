d29941cf96b2715332f91089808c5be0c34e1e05
Yap <microbious101@gmail.com>
2024-11-18 20:05:05 +1300

    added easy extensibility

--- docs/script.js
+++ docs/script.js
@@ -1,17 +1,26 @@
-
-// PLACE NEW FILENAMES IN HERE
-const heads = ["images/head1.png", "images/head2.png", "images/head3.png", "images/head4.png", "images/head5.png", "images/head6.png", "images/head7.png", "images/head8.png"];
-const bodies = ["images/body1.png", "images/body2.png", "images/body3.png", "images/body4.png", "images/body5.png", "images/body6.png", "images/body7.png", "images/body8.png"];
-const tails = ["images/tail1.png", "images/tail2.png", "images/tail3.png", "images/tail4.png", "images/tail5.png", "images/tail6.png", "images/tail7.png", "images/tail8.png"];
+// Change the number depending on how many complete creature sets there are
+var number_of_sets = 11;
+//
 
 
+var heads = [];
+var bodies = [];
+var tails = [];
+
+var template = "images/";
 
 const headImage = document.getElementById("head");
 const bodyImage = document.getElementById("body");
 const tailImage = document.getElementById("tail");
 
+function addFilenames() {
+    for (var i = 1; i <= number_of_sets; i++) {
+        heads.push(template + "head" + i + ".png");
+        bodies.push(template + "body" + i + ".png");
+        tails.push(template + "tail" + i + ".png");
+    }
+}
 
-function randomizeImages() {
     headImage.src = heads[Math.floor(Math.random() * heads.length)];
     bodyImage.src = bodies[Math.floor(Math.random() * bodies.length)];
     tailImage.src = tails[Math.floor(Math.random() * tails.length)];
@@ -20,3 +29,4 @@ function randomizeImages() {
 
 
 document.getElementById("randomizeButton").addEventListener("click", randomizeImages);
+window.addEventListener("load", addFilenames);
